bf43f564ea0d78d1d7d95212e373b5d5768fc9a7
Sandro Gargiulo <sandrogargiulo@gmail.com>
2024-11-06 14:38:02 +0100

    second commit with push

--- app/routes.py
+++ app/routes.py
@@ -8,3 +8,8 @@ main = Blueprint('main', __name__)
 def home():
     return render_template('index.html', today=datetime.today().strftime("%m/%d/%Y"))
 
+# pagina per la ricerca della minifigura
+@main.route("/insert_bl")
+def insert_bl():
+    return  render_template("insert_bl.html")
+
